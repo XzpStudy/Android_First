4af7877ec4cb81f658de885a3e5319d652ff6276
XzpStudy <52540397+XzpStudy@users.noreply.github.com>
2019-12-25 19:35:33 +0800

    完成喜马拉雅SDK的初步集成

--- app/src/main/java/com/example/fm/MainActivity.java
+++ app/src/main/java/com/example/fm/MainActivity.java
@@ -2,13 +2,50 @@ package com.example.fm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.VoiceInteractor;
 import android.os.Bundle;
+import android.service.voice.VoiceInteractionSession;
+import android.util.Log;
+
+import com.ximalaya.ting.android.opensdk.datatrasfer.CommonRequest;
+import com.ximalaya.ting.android.opensdk.datatrasfer.IDataCallBack;
+import com.ximalaya.ting.android.opensdk.model.category.Category;
+import com.ximalaya.ting.android.opensdk.model.category.CategoryList;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Map<String,String> map = new HashMap<>();
+        CommonRequest.getCategories(map, new IDataCallBack<CategoryList>() {
+            @Override
+            public void onSuccess(CategoryList categoryList) {
+                List<Category> categories = categoryList.getCategories();
+                if(categories != null){
+                    int size = categories.size();
+                    Log.d(TAG, "category size ------>" + size);
+                    for (Category category:categories) {
+                        Log.d(TAG, "category  -----<" + category.getCategoryName());
+                    }
+                }
+            }
+
+            @Override
+            public void onError(int i, String s) {
+                Log.e(TAG, "errorCode --->" + i +"errorMsg --->"+ s);
+            }
+        });
+
     }
+
 }
